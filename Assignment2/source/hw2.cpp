e995038d92acc609ba80838daa830bbe1941adaf
okra <example@com>
2022-10-15 10:51:58 +0000

    add log move

--- Assignment2/source/hw2.cpp
+++ Assignment2/source/hw2.cpp
@@ -4,25 +4,28 @@
 #include <string.h>
 #include <unistd.h>
 #include <time.h>
-#include <curses.h>
+// #include <curses.h>
 #include <termios.h>
 #include <fcntl.h>
 
+#include <iostream>
+
 #define ROW 10
-#define COLUMN 50 
+#define COLUMN 50
+#define LOG_NUM 9
+struct Node
+{
+	int x, y;
+	Node(int _x, int _y) : x(_x), y(_y){};
+	Node(){};
+} frog;
 
+char map[ROW + 10][COLUMN];
+Node logs_pos[LOG_NUM];
 
-struct Node{
-	int x , y; 
-	Node( int _x , int _y ) : x( _x ) , y( _y ) {}; 
-	Node(){} ; 
-} frog ; 
-
-
-char map[ROW+10][COLUMN] ; 
-
-// Determine a keyboard is hit or not. If yes, return 1. If not, return 0. 
-int kbhit(void){
+// Determine a keyboard is hit or not. If yes, return 1. If not, return 0.
+int kbhit(void)
+{
 	struct termios oldt, newt;
 	int ch;
 	int oldf;
@@ -42,7 +45,7 @@ int kbhit(void){
 	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 	fcntl(STDIN_FILENO, F_SETFL, oldf);
 
-	if(ch != EOF)
+	if (ch != EOF)
 	{
 		ungetc(ch, stdin);
 		return 1;
@@ -50,52 +53,70 @@ int kbhit(void){
 	return 0;
 }
 
-
-void *logs_move( void *t ){
+void *logs_move(void *index)
+{
+	int a = *((int *)index);
+	while (true)
+	{
+		sleep(1);
+		map[logs_pos[a].x][logs_pos[a].y] = ' ';
+		map[logs_pos[a].x][(logs_pos[a].y + 5) % (COLUMN - 1)] = '=';
+		logs_pos[a].y = (logs_pos[a].y + 1) % (COLUMN - 1);
+	}
 
 	/*  Move the logs  */
 
-
 	/*  Check keyboard hits, to change frog's position or quit the game. */
 
-	
 	/*  Check game's status  */
 
-
 	/*  Print the map on the screen  */
-
-	
 }
 
-int main( int argc, char *argv[] ){
+int main(int argc, char *argv[])
+{
 
 	// Initialize the river map and frog's starting position
-	memset( map , 0, sizeof( map ) ) ;
-	int i , j ; 
-	for( i = 1; i < ROW; ++i ){	
-		for( j = 0; j < COLUMN - 1; ++j )	
-			map[i][j] = ' ' ;  
-	}	
+	memset(map, 0, sizeof(map));
+	int i, j;
+	for (i = 1; i < ROW; ++i)
+	{
+		for (j = 0; j < COLUMN - 1; ++j)
+			map[i][j] = ' ';
+	}
 
-	for( j = 0; j < COLUMN - 1; ++j )	
-		map[ROW][j] = map[0][j] = '|' ;
+	for (j = 0; j < COLUMN - 1; ++j)
+		map[ROW][j] = map[0][j] = '|';
 
-	for( j = 0; j < COLUMN - 1; ++j )	
-		map[0][j] = map[0][j] = '|' ;
+	for (j = 0; j < COLUMN - 1; ++j)
+		map[0][j] = map[0][j] = '|';
 
-	frog = Node( ROW, (COLUMN-1) / 2 ) ; 
-	map[frog.x][frog.y] = '0' ; 
+	frog = Node(ROW, (COLUMN - 1) / 2);
+	map[frog.x][frog.y] = '0';
 
-	//Print the map into screen
-	for( i = 0; i <= ROW; ++i)	
-		puts( map[i] );
+	for (i = 0; i < LOG_NUM; ++i)
+	{
+		logs_pos[i] = Node(i + 1, 0);
+		for (j = 0; j < 5; j++)
+			map[logs_pos[i].x][logs_pos[i].y + j] = '=';
+	}
 
-
+	for (i = 0; i <= ROW; ++i)
+		puts(map[i]);
+	int indexs[] = {0, 1, 2, 3, 4, 5, 6, 7, 8};
 	/*  Create pthreads for wood move and frog control.  */
-
-	
+	pthread_t frog_ctl, logs[LOG_NUM];
+	// for (i = 0; i < LOG_NUM; i++)
+	// pthread_create(&logs[i], NULL, logs_move, &indexs[i]);
+	pthread_create(&logs[i], NULL, logs_move, &indexs[0]);
 	/*  Display the output for user: win, lose or quit.  */
+	// Print the map into screen
+	while (true)
+	{
+		sleep(1);
+		for (i = 0; i <= ROW; ++i)
+			puts(map[i]);
+	}
 
 	return 0;
-
 }
