559a7cd092d6c398e8488f04324c53c73b156e0e
3150 <111>
2022-11-13 07:02:22 -0500

    init

--- Assignment4/source/file_system.cu
+++ Assignment4/source/file_system.cu
@@ -25,6 +25,18 @@ __device__ void fs_init(FileSystem *fs, uchar *volume, int SUPERBLOCK_SIZE,
   fs->MAX_FILE_NUM = MAX_FILE_NUM;
   fs->MAX_FILE_SIZE = MAX_FILE_SIZE;
   fs->FILE_BASE_ADDRESS = FILE_BASE_ADDRESS;
+  // init free space management
+  for (int i = 0; i < 1024; i++) {
+    *volume++ = '\0';
+  }
+  /*
+    FCB structrue
+      byte 0: create time
+      byte 1: last modified
+      byte 2: size
+      byte 3~22: name 
+  */
+  int fcb_base = fs->SUPERBLOCK_SIZE;
 
 }
 
@@ -39,14 +51,31 @@ __device__ u32 fs_open(FileSystem *fs, char *s, int op)
 __device__ void fs_read(FileSystem *fs, uchar *output, u32 size, u32 fp)
 {
 	/* Implement read operation here */
+  for (int i = 0; i < size; i++) {
+    output[i] = fs->volume[fp+i];
+  }
 }
 
 __device__ u32 fs_write(FileSystem *fs, uchar* input, u32 size, u32 fp)
 {
+
+  // for (int i =0; i < 1024; i++) {
+  //   printf("%c\n", ptr[i]);
+  // }
+  for (int i = 0; i < size+1; i++) {
+    fs->volume[fp + i] = input[i];
+  }
 	/* Implement write operation here */
 }
 __device__ void fs_gsys(FileSystem *fs, int op)
 {
+  if (op == LS_D) {
+
+  }
+  
+  else if (op == LS_S) {
+
+  }
 	/* Implement LS_D and LS_S operation here */
 }
 
